37501a4515d12599a4608c1b10157d7cdd712dee
JiHun-star <rlarkspx@gmail.com>
2022-08-14 22:04:54 +0900

    nav 기본틀

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,50 @@
-<nav>
-  <a href="/">home</a>
-  <a href="/team">팀 기획</a>
-  <a href="/compilation">합작 기획</a>
-  <a href="/etc">개인 참여작</a>
-</nav>
-<slot></slot>
+<script>
+	let xm = -10;
+	function navbtn() {
+		if (xm == -10) {
+			xm = 0;
+		} else {
+			xm = -10;
+		}
+	}
+</script>
+
+<header style="margin-left: {xm}em;">
+	<div class="inline-block">
+		<nav class="flex flex-col">
+			<a href="/"><h2>소개</h2></a>
+			<a href="/team"><h2>팀 기획</h2></a>
+			<a href="/compilation"><h2>합작 기획</h2></a>
+			<a href="/etc"><h2>개인 참여작</h2></a>
+		</nav>
+	</div>
+	<button on:click={navbtn}>버튼</button>
+</header>
+<slot />
+
+<style>
+	header {
+		position: fixed;
+		top: 0;
+		left: 0;
+		float: left;
+
+		transition: all 250ms ease-out;
+	}
+	nav {
+		text-align: right;
+		transition: all 0.3s;
+
+		width: 10em;
+		height: 100vh;
+		background-color: #222;
+
+		padding: 1em;
+		gap: 0.5em;
+	}
+	button {
+		margin: 0;
+		padding: 0;
+		display: inline-block;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,18 @@
-<svelte:head>
-  <title>Byeol Dasoni's projects : 별다소니의 기획 및 작사 소개</title>
-</svelte:head>
-
 <script>
-  import "../app.css"; //tailwindcss
+	import '../app.css'; //tailwindcss
 </script>
 
-<h1>HOME</h1>
-<p>메인 홈입니다.</p>
+<svelte:head>
+	<title>Byeol Dasoni's projects : 별다소니의 기획 및 작사 소개</title>
+</svelte:head>
 
-<style></style>
+<section>
+	<h1>HOME</h1>
+	<p>메인 홈입니다.</p>
+</section>
+
+<style>
+	p {
+		padding: 20vw;
+	}
+</style>
