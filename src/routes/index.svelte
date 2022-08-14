c4efd89da408ec3f0d3708b38cb84468c4e15169
JiHun-star <rlarkspx@gmail.com>
2022-08-14 15:56:37 +0900

    스벨트킷 생성

---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<script>
+  import "../app.css";
+</script>
+<h1 class="font-serif ...">Welcome to SvelteKit</h1>
+<p class="font-mono ...">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
