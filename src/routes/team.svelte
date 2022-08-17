279228ec9734cffbae405231d3b03a68d1dd519e
JiHun-star <rlarkspx@gmail.com>
2022-08-17 15:35:43 +0900

    snap test

--- src/routes/team.svelte
+++ src/routes/team.svelte
@@ -4,12 +4,49 @@
 <svelte:head>
 	<title>팀 기획</title>
 </svelte:head>
-
-<h1>팀 기획</h1>
-<p>별다소니가 기획한 팀의 작품들입니다.</p>
+<main>
+	<section>1</section>
+	<section>2</section>
+	<section>3</section>
+	<section>4</section>
+</main>
 
 <style>
 	@media screen and (min-width: 320px) {
+		/* 부모 App */
+		main {
+			scroll-snap-type: y mandatory;
+			overflow-y: scroll;
+			-ms-overflow-style: none; /* IE and Edge 스크롤바 제거 */
+			scrollbar-width: none; /* Firefox 스크롤바 제거 */
+		}
+
+		main::-webkit-scrollbar {
+			display: none; /* Chrome 스크롤바 제거 */
+		}
+
+		/* 자식 section들 */
+		section {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			width: 100vw;
+			height: 100vh;
+			scroll-snap-align: start;
+			color: #fff;
+			font-size: 3rem;
+			font-weight: bold;
+		}
+
+		/* 홀수번째 자식 */
+		section:nth-child(odd) {
+			background-color: cadetblue;
+		}
+
+		/* 짝수번째 자식 */
+		section:nth-child(even) {
+			background-color: lightcoral;
+		}
 	}
 
 	@media screen and (min-width: 768px) {
