3421f8b51aa43992f6de09927c41505eb7339dd3
JiHun-star <rlarkspx@gmail.com>
2022-11-02 18:43:45 +0900

    top버튼

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,20 @@
 	import './styles.css';
 </script>
 
-<div class="app">
+<div class="app" id="app">
 	<main>
 		<slot />
 	</main>
+
+	<div class="topBtn">
+		<a href="#app" class="hover:text-primary active:text-primary-focus">
+			<i class="fa-solid fa-square-caret-up fa-3x" />
+		</a>
+	</div>
+
+	<footer>
+		<p>Copyright <i class="fa-regular fa-copyright" /> 2022 by Byeol Dasoni.</p>
+	</footer>
 </div>
 
 <style>
@@ -13,5 +23,11 @@
 		margin: 0 auto;
 		width: 82vw;
 		text-align: center;
+	}
+
+	.topBtn {
+		position: fixed;
+		bottom: 10px;
+		right: 20px;
 	}
 </style>
