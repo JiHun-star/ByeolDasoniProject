e2dfb48b9e915c73806254ae2aba28fe6b56dfa1
JiHun-star <rlarkspx@gmail.com>
2023-03-08 14:43:14 +0900

    마무리

--- src/routes/voice/+page.svelte
+++ src/routes/voice/+page.svelte
@@ -82,7 +82,7 @@
 		>
 			<h1 class="text-3xl text-base-content">WHAT is VO!CE ?</h1>
 			<br />
-			<h4 class="text-base-content">
+			<p class="text-base-content">
 				보컬 캐릭터(보컬로이드) 및 음성합성엔진을 이용하시는
 				<br />한국의 서브컬쳐 음악 프로듀서 분들의 홍보를 미약하게나마 돕고자 만들어진 채널입니다.
 				<br /><br />VO!CE에 가입된 유튜브 채널을 구독하여, 투고된 오리지널 곡들을 수집하고
@@ -98,7 +98,7 @@
 				>
 					<i class="fa-solid fa-pen-to-square" /> 한국 보카로P 신곡 소개 VO!CE 가입 신청서
 				</a>
-			</h4>
+			</p>
 		</div>
 	{/if}
 	<div class="divider" />
@@ -116,7 +116,7 @@
 		>
 			<h1 class="text-3xl text-base-content">VO!CE 가입시 참고사항</h1>
 			<br />
-			<h4 class="text-base-content">
+			<p class="text-base-content">
 				신청서 제출 시, VO!CE에서 여러분의 채널을 구독하며,
 				<br />매월 여러분의 유튜브 채널에 업로드된 영상을 분석하여 월간 'VO!CE CHART' 영상 리스트에
 				올립니다.
@@ -126,11 +126,11 @@
 				<br />'월간 한국 보카로P 신곡 소개(VO!CE CHART)' 영상의 제작 및 투고 이외에는 사용되지
 				않음을 밝힙니다.
 				<br /><br />(사용한 영상 및 음원 소스는 영상 투고 이후 폐기하는 것을 원칙으로 합니다)
-			</h4>
+			</p>
 			<br /><br />
 			<h1 class="text-3xl text-base-content">신곡 수집 기준</h1>
 			<br />
-			<h4 class="text-base-content">
+			<p class="text-base-content">
 				1. VO!CE가 구독 중인 창작자의 노래를 포함하는 유튜브 영상
 				<br /><br />2. 음성합성엔진을 이용한 오리지널 곡을 포함하는 유튜브 영상
 				<br /><br />3. VO!CE가 구독 중인 채널에, 이전에 올린 오리지널 곡을 리메이크한 영상은
@@ -139,9 +139,9 @@
 				영상이 없다면 수집되지 않습니다.
 				<br /><br />5. 오리지널 곡이 창작자 본인의 채널이 아닌 다른 채널에 올라갈 경우, 제가 인식할
 				수 있는 범위에서 수집합니다. (따로 DM 주시면 확실하게 수집됩니다)
-			</h4>
+			</p>
 			<br /><br />
-			<h4 class="text-base-content">
+			<p class="text-base-content">
 				아직 여러가지 미흡한 부분이 많이 있지만 부디 너그럽게 봐주시고,
 				<br />문의사항이 있으시면 언제든지
 				<a
@@ -152,7 +152,7 @@
 				>
 					<i class="fa-brands fa-twitter" /> 트위터 DM
 				</a> 으로 말씀해주세요!
-			</h4>
+			</p>
 		</div>
 	{/if}
 	<div class="divider" />
@@ -171,7 +171,7 @@
 		>
 			<h1 class="text-3xl text-error-content">영상 시청시 주의사항</h1>
 			<br /><br />
-			<h4 class="text-xl text-error-content">
+			<p class="text-xl text-error-content">
 				<b>
 					※ VO!CE의 모든 영상은 수익을 창출하지 않으며, 영상에 사용된 모든 영상과 음원은
 					창작자분들께 사용 허락을 받았음을 밝힙니다.
@@ -188,7 +188,7 @@
 					<br />자신이 알고 있는 곡들이 누락되어 있다면, 본 채널이 아니라 창작자분께서 참여하시도록
 					말씀 부탁드립니다.
 				</b>
-			</h4>
+			</p>
 		</div>
 	{/if}
 	<div class="divider" />
@@ -207,8 +207,8 @@
 		>
 			<h1 class="text-3xl text-base-content uppercase">producers</h1>
 			<br /><br />
-			<h4>VO!CE에 가입하신 한국 보카로/음합엔 프로듀서 분들입니다.</h4>
-			<h4>이미지를 클릭하여 해당 프로듀서의 유튜브 채널로 이동해 보세요!</h4>
+			<p>VO!CE에 가입하신 한국 보카로/음합엔 프로듀서 분들입니다.</p>
+			<p>이미지를 클릭하여 해당 프로듀서의 유튜브 채널로 이동해 보세요!</p>
 			<br /><br />
 			<input type="checkbox" class="toggle" bind:checked={toggleshow} />
 			<br /><br />
@@ -236,6 +236,11 @@
 			{/if}
 		</div>
 	{/if}
+	<div class="divider" />
+	<!-- partnership -->
+	<h2 class="bg-slate-600 text-slate-50 text-left rounded-box p-3 w-full">
+		Partnership :&nbsp; ON STAGE, 동아크루
+	</h2>
 </section>
 
 <style>
