5d1d469109d0761db58f296cabc307b0eb09c558
JiHun-star <rlarkspx@gmail.com>
2023-09-01 00:10:55 +0900

    보이스 8월호

--- src/routes/voice/+page.svelte
+++ src/routes/voice/+page.svelte
@@ -42,9 +42,9 @@
 		>
 			<h1 class="text-4xl text-neutral-content">VO!CE CHART</h1>
 			<br />
-			<h1 class="text-3xl text-neutral-content">- 2023.07 -</h1>
+			<h1 class="text-3xl text-neutral-content">- 2023.08 -</h1>
 			<iframe
-				src="https://www.youtube.com/embed/64F4g7Gp1dg"
+				src="https://www.youtube.com/embed/2PdfP59teRQ?si=nhAqtvUeYegYL4Gl"
 				title="YouTube video player"
 				frameborder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
